403d8b883929d8f31566073df595bcbfeed7e94a
Andrea <pratiandrea@libero.it>
2021-10-07 11:34:06 +0200

    Creazione model Personaggio

---
+++ 02-Hxh/Model/Personaggio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_Hxh.Model
+{
+    public class Personaggio
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Potenza { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
